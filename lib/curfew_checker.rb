ef3794748f38adb6dffd9da6359e18bd9e2893c4
Allie321 <allisonroberson04@gmail.com>
2018-07-25 19:48:22 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -2,7 +2,7 @@ def simple_curfew_checker(time)
   if time >= 23 
     puts "Apparate"
   else 
-    puts "You're not in trouble!"
+    puts "You're in trouble! Better get home quick!"
   end 
 end
 simple_curfew_checker(17)
